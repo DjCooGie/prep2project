71a510c294ca84790d891e105c635f0058d2b686
DjCooGie <evansonchagwa01@gmail.com>
2019-10-28 08:28:56 +0300

    added arrays storing day,month,year and gender data,added a function

---
+++ form.js
@@ -0,0 +1,25 @@
+// Data container
+
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+
+// Function to validate form
+function formValidate() {
+  var day = document.getElementById("day").value;
+  var month = document.getElementById("month").value;
+  var year = document.getElementById("year").value;
+  var gender = document.getElementById("gender").value;
+
+  
+}
+
+
+
+
+
+
+
+
+}
