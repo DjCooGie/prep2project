59c11c0dac1f1c8dcaf52618a4fbb3169537db4e
jusinam <evansonchagwa01@gmail.com>
2021-11-04 02:11:03 +0300

    modify behavior

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#myBirthDate").mask("99/99/9999");
+});
+
+
+function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementsByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    if(myBirthday === ""){
+        document.getElementById('message').innerHTML = "<p class=\"alert alert-danger\">Submit a Valid Date!</p>";
+        $('#message').addClass("animated bounce");
+    }
+    else {
+        for(var i=0;i<myGender.length;i++){
+            if(myGender[i].checked){
+                if(myGender[i].value === "Male"){
+                    document.getElementById('message').innerHTML = "<span><i class=\"fa fa-male fa-1x\"></i></span>&nbsp;&nbsp;<span>" + maleAkanNames[dayOfTheWeek] + "</span>" + "&nbsp;&nbsp; Born on&nbsp;&nbsp" + days[dayOfTheWeek];
+                    $('#message span:first-child').addClass("animated fadeInDown");
+                    $('#message span:last-child').addClass("animated fadeInUp");
+                }
+                else {
+                    document.getElementById('message').innerHTML = "<span><i class=\"fa fa-female\"></i></span>&nbsp;&nbsp;<span>" + femaleAkanNames[dayOfTheWeek] + "</span>" + "&nbsp;&nbsp; Born on&nbsp;&nbsp" + days[dayOfTheWeek];
+                    $('#message span:first-child').addClass("animated fadeInDown");
+                    $('#message span:last-child').addClass("animated fadeInUp");
+                }
+                break;
+            }    
+            else {
+                document.getElementById('message').innerHTML = "<p class=\"alert alert-danger\">Select Gender!</p>";
+                $('#message').addClass("animated bounce");
+            }
+        }
+    }
+}
+
+function clearAkanMessage(){
+    document.getElementById('message').innerHTML = "";
+}
